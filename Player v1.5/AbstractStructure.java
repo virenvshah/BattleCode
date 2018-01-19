fd60e722894e5f7cf5758988fee456ccacbaacf2
vvs24 <vvs24@cornell.edu>
2018-01-18 23:09:02 -0500

    2 more versions

---
+++ Player v1.5/AbstractStructure.java
@@ -0,0 +1,33 @@
+import bc.*;
+
+/**
+ * Represents a structure
+ * @author virsain
+ *
+ */
+public abstract class AbstractStructure extends AbstractUnit{
+	public enum State {Produce, Unload, Bankrupt, Blueprint, Idle};
+	
+	public State state;
+	public State previousState;
+	
+	public MapLocation currentLocation; // the location of the structure
+	public GameController gc;
+	public Map battleMap;
+	/**
+	 * Creates a new AbstractStructure
+	 * @param id
+	 * 	The blueprint ID
+	 * @param location
+	 * 	The location of the blueprint
+	 * @param structure
+	 * 	The structure type
+	 */
+	public AbstractStructure(int id, GameController g, Map map, MapLocation location, UnitType structure) {
+		this.id = id;
+		currentLocation = location;
+		type = structure;
+		gc = g;
+		battleMap = map;
+	}
+}
---
+++ Player v1.6/AbstractRobot.java
@@ -0,0 +1,266 @@
+import bc.*;
+
+/**
+ * Super class for all Robots
+ * @author virsain
+ *
+ */
+public abstract class AbstractRobot extends AbstractUnit {
+	public enum State { Build, Move, Mine, Idle; };
+	private int range[] = {72, 50, 32, 18, 8, 1};
+	
+	GameController gc;  // the game controller for the game
+	public MapLocation[] movePath;  // the path along which the robot must move
+	public Direction moveDirection;  // the direction along which the robot must move
+	public int moveIndex;  // the index of movePath, the number of steps the robot
+						 // has already taken along the path
+	MapLocation currentLocation; // the robot's current location
+	Map battleMap;  // A grid of TileNodes representing the full map
+	public State state; // The robot's present state
+	public State previousState;  // The robot's previous state
+	private int attempts;  // times trying to go to same mapLocation
+
+	public AbstractRobot(int i, GameController g, Map map, MapLocation location,
+			UnitType t) {
+		id = i;
+		gc = g;
+		battleMap = map;
+		moveIndex = 0;
+		currentLocation = location;
+		type = t;
+		
+	}
+	
+	/**
+	 * Tries to move the robot along a path.
+	 * @return
+	 * 	Returns -1 if no path is set
+	 * 	Returns  0 if successfully moved
+	 * 	Returns  1 if successfully moved and reached destination
+	 * 	Returns  2 if not ready to move (still on cooldown)
+	 * 	Returns  3 if path is blocked
+	 * 	Returns  4 if path was blocked too many times and path is reset
+	 */
+	public int movePath() {
+		// no path is set
+		if (movePath == null) {
+			state = State.Idle;
+			return -1;
+		}
+			
+		previousState = State.Move;
+		
+		// movement cooldown still up
+		if (!gc.isMoveReady(id)) {
+			// worker is still moving
+			state = State.Move;
+			return 2; 
+		}
+		
+		// get the next step's direction
+		Direction dir = currentLocation.directionTo(movePath[moveIndex]);
+		
+		// check if the robot can move in that direction
+		if (!gc.canMove(id, dir)) {
+			attempts++;
+			// number of times trying to move to a tile unsuccessfully.
+			if (attempts < 5) {
+				state = State.Move;
+				return 3;
+			} else {
+				System.out.println("Reset path");
+				movePath = null;
+				moveIndex = 0;
+				state = State.Idle;
+				return 4;
+			}
+		}
+		
+		moveDirection = dir;
+		gc.moveRobot(id, dir);
+		attempts = 0;
+		// set previous location's occupant to 0
+		battleMap.updateOccupant(currentLocation, 0);
+		// update the current location
+		currentLocation = movePath[moveIndex];
+		// update the map to the new location of the robot
+		battleMap.updateOccupant(currentLocation, id);
+		
+		moveIndex++;
+		
+		// if reached destination then reset path
+		if (moveIndex == movePath.length) {
+			movePath = null;
+			moveIndex = 0;
+			state = State.Idle;
+			return 1;
+		}
+		
+		state = State.Move;
+		return 0;
+	}
+	
+	/**
+	 * Tries to move the robot in a particular direction
+	 * 
+	 * @param dir
+	 * 	The direction in which the robot must move
+	 * @return
+	 * 	Returns -1 if no direction set
+	 * 	Returns 1 if successfully moved
+	 * 	Returns 2 if not ready to move (still on cooldown)
+	 * 	Returns 3 if path is blocked
+	 */
+	public int moveDir() {
+		if (moveDirection == null) return -1;
+		
+		previousState = State.Move;
+		
+		// movement cooldown still up
+		if (!gc.isMoveReady(id)) {
+			// worker is still trying to moves
+			state = State.Move;
+			return 2; 
+		}
+		
+		// check if the robot can move in that direction
+		if (!gc.canMove(id, moveDirection)) {
+			state = State.Move;
+			return 3;
+		}
+		
+		gc.moveRobot(id, moveDirection);
+		// set previous location's occupantId to 0
+		battleMap.updateOccupant(currentLocation, 0);
+		// update the current location
+		currentLocation = currentLocation.add(moveDirection);
+		// update the map to the new location of the robot
+		battleMap.updateOccupant(currentLocation, id);
+		
+		state = State.Idle;
+		return 1;
+	}
+	
+	/**
+	 * Finds the shortestPath between the start and end locations and sets
+	 * the robot's movePath to that path
+	 * 
+	 * @param start
+	 * 	The start location
+	 * @param end
+	 * 	The end location
+	 * 
+	 * @return 
+	 * 	True if there is a path between the start and end locations, false
+	 * 	if no path exists.
+	 */
+	public boolean setPath(MapLocation start, MapLocation end) {
+		MapLocation[] path = battleMap.shortestPath(start, end);
+		
+		// if path is empty, then no path exists
+		if (path.length == 0) return false;
+		
+		movePath = path;
+		moveIndex = 0;
+		return true;
+	}
+	
+	public void setDirection(Direction dir) {
+		moveDirection = dir;
+	}
+	
+	/**
+	 * Attempts to attack a target
+	 * 
+	 * @return
+	 * 	Returns 1 if successfully attacked
+	 * 	Returns 2 if attack cooldown still up
+	 * 	Returns 3 if units detected but too far to attack
+	 * 	Returns 4 if no units detected
+	 */
+	public int attack() {
+		// make sure attack isn't on cooldown
+		if (gc.unit(id).attackHeat() >= 10) {
+			return 2; 
+		}
+			
+		// get the units within attacking range
+		VecUnit vecUnit = gc.senseNearbyUnitsByTeam(currentLocation, 
+				gc.unit(id).attackRange(), battleMap.getEnemyTeamColor());
+		
+		// if there are no units
+		if (vecUnit.size() == 0) {
+			return 4;
+		}
+		
+		// iterate through the units and try to attack factory
+		for (int i = 0; i < vecUnit.size(); i++) {
+			Unit enemyUnit = vecUnit.get(i);
+			
+			if (enemyUnit.unitType() != UnitType.Factory) continue;
+			
+			if (gc.canAttack(id, enemyUnit.id())) {
+				gc.attack(id, enemyUnit.id());
+				System.out.println("SUCCESSFULLY ATTACKED FACTORY");
+				return 1;
+			}
+		}
+		
+		// iterate through the units and attack first one that can be attacked
+		for (int i = 0; i < vecUnit.size(); i++) {
+			Unit enemyUnit = vecUnit.get(i);
+			
+			if (gc.canAttack(id, enemyUnit.id())) {
+				gc.attack(id, enemyUnit.id());
+				System.out.println("SUCCESSFULLY ATTACKED");
+				return 1;
+			}
+		}
+		
+		return 3;
+	}
+	
+	/**
+	 * Gets the direction the robot must move to reach the closest enemy
+	 */
+	public Direction enemyDirection() {
+		// get the enemies in your vision radius
+		VecUnit vecUnit1 = gc.senseNearbyUnitsByTeam(currentLocation, 
+				gc.unit(id).visionRange(), battleMap.getEnemyTeamColor());
+		VecUnit vecUnit2;
+		
+		int i = 0;
+		
+		if (vecUnit1.size() == 0) return null;
+		
+		for (; range[i] > gc.unit(id).visionRange(); i++) ;
+	
+		// keep shrinking the range to find the enemy in the closest range
+		for (; i < range.length; i++) {
+			if (range[i] < gc.unit(id).attackRange()) break;
+			
+			vecUnit2 = gc.senseNearbyUnitsByTeam(currentLocation, 
+					range[i], battleMap.getEnemyTeamColor());
+			
+			if (vecUnit2.size() == 0) {
+				return currentLocation.directionTo(vecUnit1.get(0).
+						location().mapLocation());
+			}
+			
+			vecUnit1 = vecUnit2;
+		}
+		
+		// if vecUnit2.size() is still not zero then that means that the enemy
+		// is within attacking radius.  Hence don't move.
+		return Direction.Center;
+	}
+	
+	public MapLocation getLocation() {
+		return currentLocation;
+	}
+
+	public void idle() {
+		previousState = state;
+		state = State.Idle;
+	}
+}
