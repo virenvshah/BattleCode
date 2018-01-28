249574175a9917a8acc818935bd65092c4bc82b4
vvs24 <vvs24@cornell.edu>
2018-01-28 15:33:23 -0500

    version 4.2

---
+++ Player v4.2/Player.java
@@ -0,0 +1,190 @@
+import bc.*;
+
+/**
+ * A player that builds factories
+ * @author virsain
+ *
+ */
+public class Player {
+	GameController gc;
+	TroopManagement tm;
+	UnitCounter uc;
+
+	public static void main(String args[]) {
+		Player player= new Player();
+		player.gc = new GameController();
+		player.uc = new UnitCounter();
+		player.tm = new TroopManagement(player.gc, player.uc);
+
+		player.gc.queueResearch(UnitType.Ranger);
+		player.gc.queueResearch(UnitType.Ranger);
+		player.gc.queueResearch(UnitType.Healer);
+		player.gc.queueResearch(UnitType.Healer);
+		player.gc.queueResearch(UnitType.Rocket);
+		player.gc.queueResearch(UnitType.Rocket);
+
+		try {
+			player.tm.initializeWorkers(player.gc);
+		} catch (Exception e) {
+   		System.out.println("Exception was thrown :(");
+   		e.printStackTrace();
+   	}
+
+		player.gc.nextTurn();
+
+	   while (true) {
+	   	System.out.println("Current Round: " + player.gc.round() + " Time left " +
+	   			player.gc.getTimeLeftMs());
+	   	/* if an exception occurs we don't want the program to crash because
+	   	 * we will lose the game
+	   	 */
+	   	try {
+	   		// get the list of units every turn
+	   		VecUnit vecUnit = player.gc.myUnits();
+
+	   		// iterate through the list of units
+	      	for (int i = 0; i < vecUnit.size(); i++) {
+	      		Unit u = vecUnit.get(i);
+
+	      		AbstractUnit unit = player.tm.getUnit(u.id());
+
+	      		if (player.gc.planet() == Planet.Earth) {
+   	      		if (unit == null) {
+   	      			continue;
+   	      		}
+
+      	     		if (player.gc.round() < 400) {
+      	     			player.earthBattle(unit);
+      	     		} else if (player.gc.round() < 500){
+      	     			player.buildRockets(unit);
+      	     		} else {
+      	     			try {
+      	     				player.gc.unit(u.id());
+      	     			} catch(Exception e) {
+      	     				System.out.println("Unit died");
+      	     				continue;
+      	     			}
+      	     			player.evacuate(unit);
+      	     		}
+   	      	} else {
+   	      		if (unit == null) {
+   	      			if (u.unitType() == UnitType.Rocket) {
+   	      				player.tm.addUnit(u);
+   	      				unit = player.tm.getUnit(u.id());
+   	      			} else {
+   	      				continue;
+   	      			}
+   	      		}
+
+   	      		player.marsBattle(unit);
+   	      	}
+	      	}
+
+	      	player.tm.removeDeadWorkersAndStructures();
+	      	player.uc.resetCount();
+	   	} catch (Exception e) {
+	   		System.out.println("Exception was thrown :(");
+	   		e.printStackTrace();
+	   	}
+
+	   	player.gc.nextTurn();
+	   }
+	}
+
+	/**
+	 * Troop management for the initial stages of the game
+	 * @param unit
+	 */
+	public void earthBattle(AbstractUnit unit) {
+		// check the time of unit
+		if (unit.type == UnitType.Worker) {
+			uc.workC++;
+			tm.updateWorkerAndStructure(unit);
+			Worker worker = (Worker) unit;
+			tm.work(worker, UnitType.Factory);
+		} else if (unit.type == UnitType.Factory) {
+			uc.factC++;
+			tm.updateWorkerAndStructure(unit);
+			tm.produceDifferentUnits((Factory) unit);
+		} else if (unit.type == UnitType.Healer) {
+			uc.healC++;
+			tm.goToEnemyHealer((Healer) unit);
+		} else if (unit.type == UnitType.Ranger) {
+			uc.rangeC++;
+			tm.goToEnemy((Ranger) unit);
+		} else if (unit.type == UnitType.Knight) {
+			tm.goToEnemyKnight((Knight) unit);
+		}
+	}
+
+	public void buildRockets(AbstractUnit unit) {
+		if (unit.type == UnitType.Worker) {
+			uc.workC++;
+			tm.updateWorkerAndStructure(unit);
+			Worker worker = (Worker) unit;
+			tm.work(worker, UnitType.Rocket);
+		} else if (unit.type == UnitType.Factory) {
+			tm.updateWorkerAndStructure(unit);
+			tm.produceLess((Factory) unit, UnitType.Ranger);
+		} else if (unit.type == UnitType.Healer) {
+			uc.healC++;
+			tm.goToEnemy((Healer) unit);
+		} else if (unit.type == UnitType.Ranger) {
+			uc.rangeC++;
+			tm.goToEnemy((Ranger) unit);
+		} else if (unit.type == UnitType.Rocket) {
+			tm.updateWorkerAndStructure(unit);
+			System.out.println("Garrisoned units " + ((Rocket) unit).numOfGarrisonUnits());
+			if(((Rocket)unit).atMaxCap()){ // launch at maximum capacity
+				tm.launchRocket((Rocket) unit);
+			}
+		}
+	}
+
+	/**
+	 * Troop management to leave Earth
+	 * @param unit
+	 */
+	public void evacuate(AbstractUnit unit) {
+		if (unit.type == UnitType.Worker) {
+			uc.workC++;
+			tm.updateWorkerAndStructure(unit);
+			Worker worker = (Worker) unit;
+			tm.work(worker, UnitType.Rocket);
+		} else if (unit.type == UnitType.Factory) {
+			tm.updateWorkerAndStructure(unit);
+			tm.produceLess((Factory) unit, UnitType.Ranger);
+		} else if (unit.type == UnitType.Healer) {
+			tm.evacuate((Healer) unit);
+		} else if (unit.type == UnitType.Ranger) {
+			tm.evacuate((Ranger) unit);
+		} else if (unit.type == UnitType.Rocket) {
+			tm.updateWorkerAndStructure(unit);
+			System.out.println("Garrisoned units " + ((Rocket) unit).numOfGarrisonUnits());
+			if(((Rocket)unit).atMaxCap() || gc.round() == 745 ||
+					gc.unit(unit.id).health() < gc.unit(unit.id).maxHealth() / 2){ // launch at maximum capacity or half health
+				tm.launchRocket((Rocket) unit);
+			}
+		}
+	}
+
+	public void marsBattle(AbstractUnit unit){
+		// check unit type
+		if (unit.type == UnitType.Worker){
+			tm.updateWorkerAndStructure(unit);
+			Worker worker = (Worker) unit;
+			tm.replicateWorkerMars((Worker) unit);
+			worker.randomMovement();
+		} else if (unit.type == UnitType.Rocket){
+			tm.updateWorkerAndStructure(unit);
+			Rocket rocket = (Rocket) unit;
+			System.out.println("Trying to unload from rocket");
+			tm.rocketUnload(rocket);
+			System.out.println("Unloaded from rocket");
+		} else if (unit.type == UnitType.Ranger) {
+			tm.goToEnemyMars((Ranger) unit);
+		} else if (unit.type == UnitType.Healer){ 
+			tm.goToEnemyMars((AbstractRobot) unit);
+		}
+	}
+}
---
+++ Player v4.2/UnitCounter.java
@@ -0,0 +1,94 @@
+import bc.*;
+
+public class UnitCounter {
+	// the counts from the previous turn
+	public int rangerCount;
+	public int workerCount;
+	public int healerCount;
+	public int factoryCount;
+	public int rocketCount;
+	
+	// the counts for the present turn
+	public int rangeC;
+	public int workC;
+	public int healC;
+	public int factC;
+	public int rockC;
+	
+	// the unit caps
+	public int rangerCap;
+	public int workerCap;
+	public int healerCap;
+	public int factoryCap;
+	public int rocketCap;
+	
+	
+	public UnitCounter() {
+		rangerCount = 0;
+		workerCount = 0;
+		healerCount = 0;
+		factoryCount = 0;
+		rocketCount = 0;
+		
+		rangeC = 0;
+		workC = 0;
+		healC = 0;
+		factC = 0;
+		rockC = 0;
+		
+		rangerCap = 180;
+		workerCap = 0;
+		healerCap = 60;
+		factoryCap = 7;
+		rocketCap = 10;		
+	}
+	
+	public void resetCount() {
+		rangerCount = rangeC;
+		workerCount = workC;
+		healerCount = healC;
+		factoryCount = factC;
+		rocketCount = rockC;
+		
+		rangeC = 0;
+		workC = 0;
+		healC = 0;
+		factC = 0;
+		rockC = 0;
+	}
+	
+	public int getCap(UnitType type) {
+		switch (type) {
+			case Ranger:
+				return rangerCap;
+			case Healer:
+				return healerCap;
+			case Worker:
+				return workerCap;
+			case Factory:
+				return factoryCap;
+			case Rocket:
+				return rocketCap;
+			default:
+				return 0;
+		}
+	}
+	
+	public int getCount(UnitType type) {
+		switch (type) {
+			case Ranger:
+				return rangerCount;
+			case Healer:
+				return healerCount;
+			case Worker:
+				return workerCount;
+			case Factory:
+				return factoryCount;
+			case Rocket:
+				return rocketCount;
+			default:
+				return 0;
+		}
+	}
+}
+
